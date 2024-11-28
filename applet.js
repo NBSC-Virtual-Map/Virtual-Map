525685fb9a2d017b7559e1eb001cae6411ebb7a3
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-11-28 10:42:55 +0800

    change index

--- applet.js
+++ applet.js
@@ -33,3 +33,67 @@ class LeafletMap {
 const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
 
 myMap.loadMarkersFromJson('map.json');
+
+/*// Panorama setup for campus virtual tour
+function initializePanorama() {
+    pannellum.viewer('panorama-view', {
+        type: 'equirectangular',
+        panorama: 'https://your-image-url-here.com/campus-tour.jpg', // Replace with the actual panorama image URL
+        autoLoad: true,
+        compass: true,
+        showZoomCtrl: true,
+        autoRotate: -2,
+        pitch: 10,
+        yaw: 180,
+        hfov: 110,
+        title: 'Northern Bukidnon State College Virtual Tour',
+        author: 'Virtual Tour',
+        hotSpots: [
+            {
+                pitch: 5,
+                yaw: 90,
+                type: 'info',
+                text: 'CSS Building',
+                URL: 'https://example.com/css-building'
+            },
+            {
+                pitch: -10,
+                yaw: 210,
+                type: 'info',
+                text: 'GEC Building',
+                URL: 'https://example.com/gec-building'
+            },
+            {
+                pitch: 0,
+                yaw: 45,
+                type: 'info',
+                text: 'TEP Building',
+                URL: 'https://example.com/tep-building'
+            },
+            {
+                pitch: -5,
+                yaw: 270,
+                type: 'info',
+                text: 'BSBA Building',
+                URL: 'https://example.com/bsba-building'
+            },
+            {
+                pitch: -2,
+                yaw: 150,
+                type: 'info',
+                text: 'Admin Building',
+                URL: 'https://example.com/admin-building'
+            },
+            {
+                pitch: 2,
+                yaw: 0,
+                type: 'info',
+                text: 'Covered Court',
+                URL: 'https://example.com/covered-court'
+            }
+        ]
+    });
+}
+
+window.onload = initializePanorama;
+ */
