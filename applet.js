8dcfc466fb5fadc7bc72f0bb4a286be56c805071
wawang19 <ellevera.oscarjay19@gmail.com>
2024-12-03 20:16:53 +0800

    Updated

--- applet.js
+++ applet.js
@@ -1,3 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nextBtn = document.querySelector('.next');
+    const prevBtn = document.querySelector('.prev');
+    const carousel = document.querySelector('.carousel');
+    const list = document.querySelector('.list');
+    const runningTime = document.querySelector('.carousel .timeRunning');
+    const timeRunning = 3000;
+    const timeAutoNext = 7000;
+
+    let runTimeOut;
+    let runNextAuto = setTimeout(() => {
+        nextBtn.click();
+    }, timeAutoNext);
+
+    // Navigation for slider
+    nextBtn.onclick = function () {
+        showSlider('next');
+    };
+
+    prevBtn.onclick = function () {
+        showSlider('prev');
+    };
+
+    function resetTimeAnimation() {
+        runningTime.style.animation = 'none';
+        runningTime.offsetHeight; // Trigger reflow
+        runningTime.style.animation = null;
+        runningTime.style.animation = 'runningTime 7s linear 1 forwards';
+    }
+
+    function showSlider(type) {
+        let sliderItemsDom = list.querySelectorAll('.carousel .list .item');
+        if (type === 'next') {
+            list.appendChild(sliderItemsDom[0]);
+            carousel.classList.add('next');
+        } else {
+            list.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+            carousel.classList.add('prev');
+        }
+
+        clearTimeout(runTimeOut);
+
+        runTimeOut = setTimeout(() => {
+            carousel.classList.remove('next');
+            carousel.classList.remove('prev');
+        }, timeRunning);
+
+        clearTimeout(runNextAuto);
+        runNextAuto = setTimeout(() => {
+            nextBtn.click();
+        }, timeAutoNext);
+
+        resetTimeAnimation(); // Reset the running time animation
+    }
+
+    // Start the initial animation
+    resetTimeAnimation();
+});
+
+
 class LeafletMap {
 
     constructor(containerId, center, zoom) {
