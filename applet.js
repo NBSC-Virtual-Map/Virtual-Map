2b30dcb10e7e968222d3ca9c4052246767773661
AHRDYJANEPDESALAN8888 <desalan.ahrdy08@gmail.com>
2024-12-06 14:35:11 +0800

    kalimot ko

--- applet.js
+++ applet.js
@@ -92,9 +92,8 @@ class LeafletMap {
     createPopupContent(marker) {
         return `
             <div>
-                <img src="${marker.imageUrl}" alt="Marker Image" style="width:100px;height:auto;margin-bottom:5px;">
+                <img src="${marker.imageUrl}" alt="Marker Image" style="width:200px;height:160;margin-bottom:5px;">
                 <p>${marker.text}</p>
-                <button onclick="alert('Button clicked for marker: ${marker.text}')">Click Me</button>
             </div>
         `;
     }
@@ -126,66 +125,3 @@ window.onclick = function(event) {
 
 
 
-/*// Panorama setup for campus virtual tour
-function initializePanorama() {
-    pannellum.viewer('panorama-view', {
-        type: 'equirectangular',
-        panorama: 'https://your-image-url-here.com/campus-tour.jpg', // Replace with the actual panorama image URL
-        autoLoad: true,
-        compass: true,
-        showZoomCtrl: true,
-        autoRotate: -2,
-        pitch: 10,
-        yaw: 180,
-        hfov: 110,
-        title: 'Northern Bukidnon State College Virtual Tour',
-        author: 'Virtual Tour',
-        hotSpots: [
-            {
-                pitch: 5,
-                yaw: 90,
-                type: 'info',
-                text: 'CSS Building',
-                URL: 'https://example.com/css-building'
-            },
-            {
-                pitch: -10,
-                yaw: 210,
-                type: 'info',
-                text: 'GEC Building',
-                URL: 'https://example.com/gec-building'
-            },
-            {
-                pitch: 0,
-                yaw: 45,
-                type: 'info',
-                text: 'TEP Building',
-                URL: 'https://example.com/tep-building'
-            },
-            {
-                pitch: -5,
-                yaw: 270,
-                type: 'info',
-                text: 'BSBA Building',
-                URL: 'https://example.com/bsba-building'
-            },
-            {
-                pitch: -2,
-                yaw: 150,
-                type: 'info',
-                text: 'Admin Building',
-                URL: 'https://example.com/admin-building'
-            },
-            {
-                pitch: 2,
-                yaw: 0,
-                type: 'info',
-                text: 'Covered Court',
-                URL: 'https://example.com/covered-court'
-            }
-        ]
-    });
-}
-
-window.onload = initializePanorama;
- */
